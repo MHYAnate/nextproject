7dc8e962f18533ec8572dec98a1141efe58702e9
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2023-10-21 18:24:59 +0100

    one

--- src/features/bodyCard/tabVacancy.tsx
+++ src/features/bodyCard/tabVacancy.tsx
@@ -56,16 +56,9 @@ export const VacancyTab = () => {
 			{tab === "Vacancy" && (
 				<div>
 					<div className={styles.close} onClick={() => selectTab("")}>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							id="Layer_1"
-							className={styles.svg}
-							fill="red"
-							stroke="red"
-							data-name="Layer 1"
-						>
-							<path d="m24 5a3 3 0 0 0 -3-3h-13.242l-7.758 10 7.758 10h13.242a3 3 0 0 0 3-3zm-6.041 9.543-1.414 1.414-2.545-2.543-2.543 2.543-1.414-1.414 2.543-2.543-2.543-2.543 1.414-1.414 2.543 2.543 2.543-2.543 1.414 1.414-2.543 2.543z" />
-						</svg>
+					<svg xmlns="http://www.w3.org/2000/svg" id="Isolation_Mode" data-name="Isolation Mode" viewBox="0 0 24 24"className={styles.svg}
+								fill="red"
+								stroke="red"><polygon points="24.061 2.061 21.939 -0.061 12 9.879 2.061 -0.061 -0.061 2.061 9.879 12 -0.061 21.939 2.061 24.061 12 14.121 21.939 24.061 24.061 21.939 14.121 12 24.061 2.061"/></svg>
 					</div>
 					<VacancyRender />
 				</div>
