cbc15117ea9fa8e1a2c5c883ad0a06061d9dae73
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2023-10-21 19:33:19 +0100

    check

--- src/features/bodyCard/tabEcommerce.tsx
+++ src/features/bodyCard/tabEcommerce.tsx
@@ -57,8 +57,8 @@ export const CommerceTab = () => {
 				<div>
 					<div className={styles.close} onClick={() => selectTab("")}>
 					<svg xmlns="http://www.w3.org/2000/svg" id="Isolation_Mode" data-name="Isolation Mode" viewBox="0 0 24 24"className={styles.svg}
-								fill="red"
-								stroke="red"><polygon points="24.061 2.061 21.939 -0.061 12 9.879 2.061 -0.061 -0.061 2.061 9.879 12 -0.061 21.939 2.061 24.061 12 14.121 21.939 24.061 24.061 21.939 14.121 12 24.061 2.061"/></svg>
+								fill="currentcolor"
+								stroke="currentcolor"><polygon points="24.061 2.061 21.939 -0.061 12 9.879 2.061 -0.061 -0.061 2.061 9.879 12 -0.061 21.939 2.061 24.061 12 14.121 21.939 24.061 24.061 21.939 14.121 12 24.061 2.061"/></svg>
 					</div>
 					<CommerceRender />
 				</div>
