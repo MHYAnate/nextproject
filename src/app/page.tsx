da28eb327da5a15042f79a506a307f4459b9215b
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2023-10-30 10:26:03 +0100

    responsivenes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,32 @@
 "use client";
 import Ilud from "@/features/ilud/ilud";
-import Link from "next/link";
-import styles from "./styles.module.css";
 import { Services } from "@/features/addCarousel/data";
 import InCarousel from "@/features/addCarousel/ad1";
 import AboutContainer from "@/features/aboutUs/about";
 import UserBtn from "@/features/frontUserBtn/userBtnContainer";
-import { AutomativeTab } from "@/features/bodyCard/tabAutomative";
-import { MaintainaceTab } from "@/features/bodyCard/tabMaintnce";
 import VendorCard from "@/features/bodyCard/vendorCard";
 import PlatformCard from "@/features/bodyCard/platformCard";
 import PanelCard from "@/features/bodyCard/panelCard";
-
+import styles from "./styles.module.css";
 export default function Home() {
 	return (
 		<>
 			{/*<div className="text text-green-700">BismiLLAHI RAHMAANI RAHIIM</div>*/}
 			<div className={styles.main}>
+				<div className={styles.headMain}>
 				<div className={styles.header}>
 					<div className={styles.nav}>
 						<div className={styles.inNav}>
 							<AboutContainer />
 						</div>
-						<div>
+						<div className={styles.iLudCover}>
 							<Ilud />
 						</div>
 					</div>
 				</div>
-				<div>
+				<div className={styles.panelCover}>
 					<PanelCard />
+				</div>
 				</div>
 				<div className={styles.carouselCover}>
 					<InCarousel Services={Services} />
