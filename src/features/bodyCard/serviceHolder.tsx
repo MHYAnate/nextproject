1597417f5e1a8f53167e6a85cdba4f16d745aaf9
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2023-11-08 09:20:06 +0100

    checking

--- src/features/bodyCard/serviceHolder.tsx
+++ src/features/bodyCard/serviceHolder.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-
+import React, { useTransition } from "react";
 import { Loader } from "@googlemaps/js-api-loader"
 import Image from "next/image";
 import styles from "./styles.module.css";
@@ -13,42 +12,28 @@ interface CarouselProps {
 	};
 }
 
+
 const ServiceHolder = (props: CarouselProps) => {
-  // Initialize and add the map
-let map;
-async function initMap(): Promise<void> {
-  // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
 
-  // Request needed libraries.
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
+	let startTime = performance.now();
 
-  // The map, centered at Uluru
-  map = new Map(
-    document.getElementById('map') as HTMLElement,
-    {
-      zoom: 4,
-      center: position,
-      mapId: 'DEMO_MAP_ID',
-    }
-  );
-
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerElement({
-    map: map,
-    position: position,
-    title: 'Uluru'
-  });
-}
-
-React.useEffect(() => {
+	const loader = new Loader({
+		apiKey: "AIzaSyDCN0cgiDpCY-3N5fHnIONkyep1Th8h6mk",
+		version: "weekly",
+		
+	});
 	
-		initMap();
+	loader.load().then(async () => {
+		const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+		map = new Map(document.getElementById("map") as HTMLElement, {
+			center: { lat: 9.036485, lng: 7.476240 },
+			zoom: 13,
+		});
+	});
 
+	// Initialize and add the map
+	
 
-},[map])
 	return (
 		<div className={styles.displayFinalService}>
 			<div className={styles.inDisplayFinalService}>
@@ -62,21 +47,18 @@ React.useEffect(() => {
 						unoptimized
 					/>
 				</div>
-				<div className={styles.sHName}>
-				{props.services.name} {" "} Vendors
-				</div>
+				<div className={styles.sHName}>{props.services.name} Vendors</div>
 			</div>
 			<div className={styles.body}>
 				<div id="map" className={styles.map}>
-	        <script async
-             src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDCN0cgiDpCY-3N5fHnIONkyep1Th8h6mk&callback=initMap">
-          </script>
+					<script
+						async
+						src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDCN0cgiDpCY-3N5fHnIONkyep1Th8h6mk&libraries=marker&callback=initMap"
+					></script>
+					
 				</div>
-				<div className={styles.inBody}>
-
-				</div>
+				<div className={styles.inBody}></div>
 			</div>
-		
 		</div>
 	);
 };
---
+++ src/features/bodyCard/testHolder.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { Loader } from "@googlemaps/js-api-loader";
+import Image from "next/image";
+import styles from "./styles.module.css";
+
+interface CarouselProps {
+  services: {
+    id: number;
+    name: string;
+    src: string;
+    link: string;
+  };
+}
+
+let map: google.maps.Map;
+
+function initMap(): void {
+  // The location of Uluru
+  const position = { lat: -25.344, lng: 131.031 };
+
+  // The map, centered at Uluru
+  const Map = new google.maps.Map(
+    document.getElementById('map') as HTMLElement,
+    {
+      zoom: 4,
+      center: position,
+      mapId: 'DEMO_MAP_ID',
+    }
+  );
+
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    map: Map,
+    position: position,
+    title: 'Uluru'
+  });
+}
+
+
+const ServiceHolder: React.FC<CarouselProps>= ({services})  => {
+  React.useEffect(() => {
+    initMap();
+  }, []);
+
+  return (
+    <div className={styles.displayFinalService}>
+      <div className={styles.inDisplayFinalService}>
+        <div className={styles.serviceImg}>
+          <Image
+            className={styles.imgITC}
+            src={services.src}
+            alt={services.name}
+            width={500}
+            height={500}
+            unoptimized
+          />
+        </div>
+        <div className={styles.sHName}>
+        {services.name} {" "} Vendors
+        </div>
+      </div>
+      <div className={styles.body}>
+        <div id="map" className={styles.map}></div>
+        <div className={styles.inBody}>
+        <script async
+             src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDCN0cgiDpCY-3N5fHnIONkyep1Th8h6mk&callback=initMap">
+          </script>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+ServiceHolder.displayName = "ServiceHolder";
+export default ServiceHolder;
