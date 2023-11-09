f318c31d54d361f043d3aa21c8cc3fa9ec8e474b
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2023-11-09 21:05:28 +0100

    checking map

--- src/features/bodyCard/serviceHolder.tsx
+++ src/features/bodyCard/serviceHolder.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Loader } from "@googlemaps/js-api-loader"
+import { Loader } from "@googlemaps/js-api-loader";
 import Image from "next/image";
 import styles from "./styles.module.css";
 
@@ -13,46 +13,41 @@ interface CarouselProps {
 }
 
 const ServiceHolder = (props: CarouselProps) => {
-
-
 	// Initialize and add the map
-let map;
+	let map;
 
-async function initMap(): Promise<void> {
-  // The location of area 2 section 1 oyo street block 30 flat 2 garki Abuja
-  const position = { lat: 9.036485, lng: 7.476240 };
+	async function initMap(): Promise<void> {
+		// The location of area 2 section 1 oyo street block 30 flat 2 garki Abuja
+		const position = { lat: 9.036485, lng: 7.47624 };
 
-  // Request needed libraries.
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+		// Request needed libraries.
+		//@ts-ignore
+		const { Map } = (await google.maps.importLibrary(
+			"maps"
+		)) as google.maps.MapsLibrary;
 
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
+		const { AdvancedMarkerElement } = (await google.maps.importLibrary(
+			"marker"
+		)) as google.maps.MarkerLibrary;
 
-  // The map, centered at Uluru
-  map = new Map(
-    document.getElementById('map') as HTMLElement,
-    {
-      zoom: 15,
-      center: position,
-      mapId: 'DEMO_MAP_ID',
-    }
-  );
+		// The map, centered at Uluru
+		map = new Map(document.getElementById("map") as HTMLElement, {
+			zoom: 15,
+			center: position,
+			mapId: "DEMO_MAP_ID",
+		});
 
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerElement({
-    map: map,
-    position: position,
-    title: 'Uluru'
-  });
-}
+		// The marker, positioned at Uluru
+		const marker = new AdvancedMarkerElement({
+			map: map,
+			position: position,
+			title: "Uluru",
+		});
+	}
 
-
-	setTimeout(()=>{
-    initMap();
-	}, 1000)
-	
-
-
+	setTimeout(() => {
+		initMap();
+	}, 1000);
 
 	return (
 		<div className={styles.displayFinalService}>
@@ -71,11 +66,14 @@ async function initMap(): Promise<void> {
 			</div>
 			<div className={styles.body}>
 				<div id="map" className={styles.map}>
-				<script async
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDCN0cgiDpCY-3N5fHnIONkyep1Th8h6mk&callback=initMap">
-</script>
+					<script
+						async
+						src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDCN0cgiDpCY-3N5fHnIONkyep1Th8h6mk&callback=initMap"
+					></script>
 				</div>
-				<div className={styles.inBody}></div>
+				<div className={styles.inBody}>
+					
+				</div>
 			</div>
 		</div>
 	);
