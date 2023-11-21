565824dfe65bc5074f62c07d1b55a583b0db938f
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2023-11-21 08:48:44 +0100

    navShaped

--- src/features/aboutUs/about.tsx
+++ src/features/aboutUs/about.tsx
@@ -22,13 +22,15 @@ export default function AboutContainer() {
         onClick={() =>tab === 'about' ? selectTab(''):selectTab('about')}
       >
         <div className={(tab ==='about'?styles.tabBtnclicked: styles.tabBtn)}>{tab ==='about' ? (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={styles.abtSvg}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M3 4.5h14.25M3 9h9.75M3 13.5h5.25m5.25-.75L17.25 9m0 0L21 12.75M17.25 9v12" />
-      </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={styles.abtSvg}>
+      <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+    </svg>
+    
+     
       ):(
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={styles.abtSvg}>
-          <path strokeLinecap="round" strokeLinejoin="round" d="M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0l-3.75-3.75M17.25 21L21 17.25" />
-        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={styles.abtSvg}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
+      </svg>
         )
         }
 
