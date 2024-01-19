a4631c05880b4c570b87c56a7898eb791107e762
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2024-01-19 07:51:46 +0100

    feat:burgerMenu with googleearth map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,23 @@
 import Ilud from "@/features/ilud/ilud";
 import styles from "./styles.module.css";
 import MobileNavTab from "@/features/nav/mobile/navTab";
-import { updateCountFromLength } from '@/lib/features/slice/notificationSlice'
-import {  updateMsgLength } from '@/lib/features/slice/messageSlice'
-import { useAppDispatch } from '@/lib/hooks'
-import { Notification } from '@/features/database/notifiaction';
-import { Messages } from '@/features/database/messages';
+import { updateCountFromLength } from "@/lib/features/slice/notificationSlice";
+import { updateMsgLength } from "@/lib/features/slice/messageSlice";
+import { useAppDispatch } from "@/lib/hooks";
+import { Notification } from "@/features/database/notifiaction";
+import { Messages } from "@/features/database/messages";
 import InCarousel from "@/features/carousel/mobile/carousel";
 import { Services } from "@/features/database/data";
+import BurgerMenu from "@/features/hamBurgerMenu/mobile/burgerMenu";
 
 export default function Home() {
-	const dispatch = useAppDispatch()
-	if(true){
+	const dispatch = useAppDispatch();
+	if (true) {
 		dispatch(updateCountFromLength(Notification.length));
-	 }
-	 if(true){
+	}
+	if (true) {
 		dispatch(updateMsgLength(Messages.length));
-	 }
+	}
 	return (
 		<>
 			{/*<div className="text text-green-700">BismiLLAHI RAHMAANI RAHIIM</div>*/}
@@ -25,19 +26,19 @@ export default function Home() {
 				<div className={styles.navCover}>
 					<nav className={styles.nav}>
 						<div className={styles.iludCover}>
-						<div className={styles.mobileIlud}>
-							<Ilud/>
-						</div>
-						<div className={styles.tabletIlud}>
-							<Ilud/>
-						</div>
-						<div className={styles.desktopIlud}>
-							<Ilud/>
-						</div>
+							<div className={styles.mobileIlud}>
+								<Ilud />
+							</div>
+							<div className={styles.tabletIlud}>
+								<Ilud />
+							</div>
+							<div className={styles.desktopIlud}>
+								<Ilud />
+							</div>
 						</div>
 						<div className={styles.navlist}>
 							<div className={styles.mobileNavList}>
-								<MobileNavTab/>
+								<MobileNavTab />
 							</div>
 							<div className={styles.tabletNavList}></div>
 							<div className={styles.desktopNavList}></div>
@@ -46,9 +47,11 @@ export default function Home() {
 				</div>
 				<div className={styles.mobilebody}>
 					<div className={styles.mobileCarouselCover}>
-						<InCarousel Services={Services}/>
-						</div>
-					<div className={styles.mobileBurgerMenu}></div>
+						<InCarousel Services={Services} />
+					</div>
+					<div className={styles.mobileBurgerMenu}>
+						<BurgerMenu />
+					</div>
 					<div className={styles.mobileNewLetter}></div>
 				</div>
 				<div className={styles.tabletBody}>
--- src/features/hamBurgerMenu/mobile/burgerMenu.tsx
+++ src/features/hamBurgerMenu/mobile/burgerMenu.tsx
@@ -4,17 +4,23 @@ import { MaintainaceTab } from "@/features/hamBurgerMenu/mobile/tabMaintnce";
 import styles from "./styles.module.css";
 import { Services } from "@/features/database/data";
 import { PersonalTab } from "./tabPersonal";
-const VendorCard = () => {
+import { VacancyTab } from "./tabVacancy";
+import { CommerceTab } from "./tabEcommerce";
+import { ChatTab } from "./tabChat";
+const BurgerMenu = () => {
 	return (
 		
      
-			<div className={styles.vendor}>
+			<div className={styles.burgerMenu}>
 				<AutomativeTab Services={Services} />
 				<MaintainaceTab Services={Services} />
 				<PersonalTab Services={Services} />
+				<VacancyTab/>
+				<CommerceTab/>
+				<ChatTab/>
 			</div>
 		
 	);
 };
 
-export default VendorCard;
+export default BurgerMenu;
